0c8ad36209778867763f176b8f2ddca4b4674eea
Imtiyaz Zaman <imtiyaz.zaman@live.co.uk>
2023-11-04 15:59:28 +0000

    Initial Commit

---
+++ Push Up Tracker/Push_Up_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Push_Up_TrackerApp.swift
+//  Push Up Tracker
+//
+//  Created by Imtiyaz Zaman on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Push_Up_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
