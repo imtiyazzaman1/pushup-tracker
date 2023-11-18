d3d4779eb6b0857222b016fb200cc902f54b5259
Imtiyaz Zaman <imtiyaz.zaman@live.co.uk>
2023-11-18 10:09:36 +0000

    adds simple UI with counter and save button

--- Push Up Tracker/ContentView.swift
+++ Push Up Tracker/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @State private var currentCount: Int = 0
+    @State private var counts: [Int] = []
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -17,69 +19,31 @@ struct ContentView: View {
     private var sets: FetchedResults<PushUpSet>
     
     var body: some View {
-        NavigationView {
-            List {                
-                ForEach(sets) { pSet in
-                    NavigationLink {
-                        Text("\(pSet.reps) reps at \(pSet.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(pSet.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack{
+            Text("Press Up Tracker")
+                .font(.caption)
+            Text("\(currentCount)")
+                .font(.title)
+            
+            Button(action: increment) {
+                Label("add", systemImage: "plus")
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            
+            Button("Save", action: save)
+                .disabled(currentCount == 0)
+            
+
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = PushUpSet(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    private func increment() {
+        currentCount += 1
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { sets[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    private func save() {
+        currentCount = 0
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
