384a6e0e914d252801b6333e131b591eee732a1a
Imtiyaz Zaman <imtiyaz.zaman@live.co.uk>
2023-12-04 08:56:09 +0000

    adds total count

--- Push Up Tracker/screens/HistoryScreen.swift
+++ Push Up Tracker/screens/HistoryScreen.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HistoryScreen: View {
     @State private var historyIndex = 0
     
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query(filter: PushUpSet.todayPredicate(), sort: \PushUpSet.timestamp, order: .reverse)
+    private var todaysSets:[PushUpSet]
+    @State private var total: Int = 0
+    
     var body: some View {
         TabView(selection: $historyIndex) {
             TodayHistoryView()
-                .tabItem { Text("Today") }
+                .tabItem { Text("Today (\(total))") }
                 .tag(0)
             AllHistoryView()
                 .tabItem { Text("All") }
@@ -21,10 +28,16 @@ struct HistoryScreen: View {
             // Add more views as needed
         }
         .tabViewStyle(DefaultTabViewStyle())
+        .onAppear(perform: {
+            total = todaysSets.reduce(0) { res, current in
+                res + current.reps
+            }
+        })
         
     }
 }
 
 #Preview {
     HistoryScreen()
+        .modelContainer(PreviewDataController.previewContainer)
 }
